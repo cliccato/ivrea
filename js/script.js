432fb40e42e2ec4388ae04b342c3f202f211920f
cliccato <58629115+cliccato@users.noreply.github.com>
2023-05-26 16:19:18 +0200

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-var carousel = document.getElementById('backgroundCarousel');
-  var carouselInstance = new bootstrap.Carousel(carousel, {
-    interval: 3000
-  });
-
 function showIvreaInfo() {
+  var button = document.getElementById("show-ivrea-info");
+  if (document.getElementById("ivrea-info").classList.contains("d-none")) {
     var elementsToShow = document.getElementById("ivrea-info");
     elementsToShow.classList.remove("d-none");
+    button.textContent = "Scopri meno";
+  } else {
+    var elementsToShow = document.getElementById("ivrea-info");
+    elementsToShow.classList.add("d-none");
+    button.textContent = "Scopri di più";
+  }
 }
